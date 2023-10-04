823c67682e746cc3f14a575ab347dae6b315d5bf
jason <jason@fastmail.org>
2023-10-04 12:11:42 -0600

    fft

--- preprocessing/make_dataset.py
+++ preprocessing/make_dataset.py
@@ -108,14 +108,42 @@ def preprocess(filepath, output_name, dataset_name):
     output_path = os.path.join(datasets_path, dataset_name,'processed', output_name+".csv")
     df.to_csv(output_path, index=False)
 
+
+#Fp1,Fz,F3,F7,FT9,FC5,FC1,C3,T7,ECG1,CP5,CP1,Pz,P3,P7,O1,Oz,O2,P4,P8,TP10,CP6,CP2,FCz,C4,T8,FT10,FC6,FC2,F4,F8,Fp2,AF7,AF3,AFz,F1,F5,FT7,FC3,C1,C5,TP7,CP3,P1,P5,PO7,PO3,POz,PO4,PO8,P6,P2,CPz,CP4,TP8,C6,C2,FC4,FT8,F6,AF8,AF4,F2
+
+
+def compute_fft_on_csv(input_csv_path, output_csv_path):
+    df = pd.read_csv(input_csv_path)
+    df_fft = pd.DataFrame()  # DataFrame to store FFT results
+
+    for column in df.columns:
+        # Skip non-numeric columns
+        if not np.issubdtype(df[column].dtype, np.number):
+            continue
+
+        # Convert the column to floats and create a NumPy array
+        waveform = df[column].values.astype(float)
+
+        sampling_rate = 500  # Sampling rate in Hz
+        num_samples = len(waveform)
+        frequencies = np.fft.fftfreq(num_samples, 1/sampling_rate)
+        fft_values = np.fft.fft(waveform)
+
+        # Compute magnitude of FFT values and keep only the positive frequencies
+        fft_magnitudes = np.abs(fft_values)
+        positive_freq_idxs = np.where(frequencies > 0)
+        df_fft[column] = fft_magnitudes[positive_freq_idxs]
+
+    df_fft.to_csv(output_csv_path, index=False)
+
 def main(): 
     args = parse_arguments()
     
     if args.dataset_name:
         name = args.dataset_name
     else:
-        name = "untitled_dataset"
-
+        name = "dataset_1"
+    '''
     #make directory structure of for dataset 
     make_dir_structure(name)
 
@@ -125,5 +153,12 @@ def main():
     for x in raw_file_paths:
         print(x)
         preprocess(x, ut.create_short_identifier(x), name) 
+    ''' 
+    fft_input = os.path.join(datasets_path, name,'processed')
+    processed_waveforms = ut.find_files_with_ext(".csv", fft_input)
+
+    for x in processed_waveforms:
+        fft_output = os.path.join(datasets_path, name,'feature_selected', os.path.basename(x))
+        compute_fft_on_csv(x, fft_output)
 
 main()
